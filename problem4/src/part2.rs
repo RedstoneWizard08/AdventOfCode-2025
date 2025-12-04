253d6f842c5b010ac55208a555259882cbf5be0c
RedstoneWizard08 <jacob1coder@gmail.com>
2025-12-04 14:12:50 -0800

    minor optimization

--- problem4/src/part2.rs
+++ problem4/src/part2.rs
@@ -2,35 +2,40 @@ use macros::embed_input;
 
 embed_input!("../input.txt");
 
+const WIDTH_I: isize = WIDTH as isize;
+const HEIGHT_I: isize = HEIGHT as isize;
+
+#[inline(always)]
 fn get_cell(cells: &[[bool; WIDTH]; HEIGHT], y: isize, x: isize) -> bool {
-    if x < 0 || x > WIDTH as isize - 1 || y < 0 || y > WIDTH as isize - 1 {
+    if x < 0 || x > WIDTH_I - 1 || y < 0 || y > WIDTH_I - 1 {
         false
     } else {
         cells[y as usize][x as usize]
     }
 }
 
+#[inline(always)]
 fn check(cells: &[[bool; WIDTH]; HEIGHT]) -> usize {
     let mut count = 0;
 
-    for y in 0..HEIGHT as isize {
-        for x in 0..WIDTH as isize {
-            if !get_cell(&cells, y, x) {
+    for y in 0..HEIGHT_I {
+        for x in 0..WIDTH_I {
+            if !cells[y as usize][x as usize] {
                 continue;
             }
 
-            let values = [
-                get_cell(&cells, y, x - 1),     // left
-                get_cell(&cells, y, x + 1),     // right
-                get_cell(&cells, y - 1, x),     // up
-                get_cell(&cells, y + 1, x),     // down
-                get_cell(&cells, y - 1, x - 1), // top left
-                get_cell(&cells, y - 1, x + 1), // top right
-                get_cell(&cells, y + 1, x - 1), // bottom left
-                get_cell(&cells, y + 1, x + 1), // bottom right
-            ];
+            let mut filled = 0;
 
-            if values.into_iter().filter(|it| *it).count() < 4 {
+            filled += get_cell(&cells, y, x - 1) as u8; // left
+            filled += get_cell(&cells, y, x + 1) as u8; // right
+            filled += get_cell(&cells, y - 1, x) as u8; // up
+            filled += get_cell(&cells, y + 1, x) as u8; // down
+            filled += get_cell(&cells, y - 1, x - 1) as u8; // top left
+            filled += get_cell(&cells, y - 1, x + 1) as u8; // top right
+            filled += get_cell(&cells, y + 1, x - 1) as u8; // bottom left
+            filled += get_cell(&cells, y + 1, x + 1) as u8; // bottom right
+
+            if filled < 4 {
                 count += 1;
             }
         }
@@ -44,6 +49,7 @@ pub fn main() {
     // bool => Is there paper in the cell?
     let mut cells = [[false; WIDTH]; HEIGHT];
 
+    // I *could* do this step at compile time, but that would be cheating for the benchmarks.
     for (y, line) in INPUT.iter().enumerate() {
         for (x, ch) in line.iter().enumerate() {
             cells[y][x] = *ch == '@';
@@ -54,24 +60,24 @@ pub fn main() {
     let mut removed = 0;
 
     while count > 0 {
-        for y in 0..HEIGHT as isize {
-            for x in 0..WIDTH as isize {
-                if !get_cell(&cells, y, x) {
+        for y in 0..HEIGHT_I {
+            for x in 0..WIDTH_I {
+                if !cells[y as usize][x as usize] {
                     continue;
                 }
 
-                let values = [
-                    get_cell(&cells, y, x - 1),     // left
-                    get_cell(&cells, y, x + 1),     // right
-                    get_cell(&cells, y - 1, x),     // up
-                    get_cell(&cells, y + 1, x),     // down
-                    get_cell(&cells, y - 1, x - 1), // top left
-                    get_cell(&cells, y - 1, x + 1), // top right
-                    get_cell(&cells, y + 1, x - 1), // bottom left
-                    get_cell(&cells, y + 1, x + 1), // bottom right
-                ];
+                let mut filled = 0;
 
-                if values.into_iter().filter(|it| *it).count() < 4 {
+                filled += get_cell(&cells, y, x - 1) as u8; // left
+                filled += get_cell(&cells, y, x + 1) as u8; // right
+                filled += get_cell(&cells, y - 1, x) as u8; // up
+                filled += get_cell(&cells, y + 1, x) as u8; // down
+                filled += get_cell(&cells, y - 1, x - 1) as u8; // top left
+                filled += get_cell(&cells, y - 1, x + 1) as u8; // top right
+                filled += get_cell(&cells, y + 1, x - 1) as u8; // bottom left
+                filled += get_cell(&cells, y + 1, x + 1) as u8; // bottom right
+
+                if filled < 4 {
                     cells[y as usize][x as usize] = false;
                     removed += 1;
                 }
