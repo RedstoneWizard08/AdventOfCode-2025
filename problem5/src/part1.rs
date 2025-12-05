d549fa7008692ac33ae28bba0686247cdccdb207
RedstoneWizard08 <jacob1coder@gmail.com>
2025-12-04 21:17:00 -0800

    PART 2 OF DAY 5 DOES NOT WORK

--- problem5/src/part1.rs
+++ problem5/src/part1.rs
@@ -1,3 +1,31 @@
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("../input.txt");
+
 pub fn main() {
-    println!("Hello, world!");
+    let (pre, post) = INPUT.trim().split_once("\n\n").unwrap();
+    let ranges = pre.trim().lines().map(|it| it.to_owned()).collect_vec();
+    let ids = post.trim().lines().map(|it| it.to_owned()).collect_vec();
+
+    let mut fresh = Vec::new();
+
+    for item in ranges {
+        let (start, end) = item.split_once("-").unwrap();
+        let start = start.parse::<u64>().unwrap();
+        let end = end.parse::<u64>().unwrap();
+
+        fresh.push(start..=end);
+    }
+
+    let mut ok = 0;
+
+    for id in ids {
+        let id = id.parse::<u64>().unwrap();
+
+        if fresh.iter().any(|it| it.contains(&id)) {
+            ok += 1;
+        }
+    }
+
+    println!("Fresh: {ok}");
 }
--- problem5/src/part2.rs
+++ problem5/src/part2.rs
@@ -1,3 +1,23 @@
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("../input.txt");
+
+// DOES NOT WORK
 pub fn main() {
-    println!("Hello, world!");
+    let (pre, _) = INPUT.trim().split_once("\n\n").unwrap();
+    let ranges = pre.trim().lines().map(|it| it.to_owned()).collect_vec();
+
+    let mut min = 0u64;
+    let mut max = 0u64;
+
+    for item in ranges {
+        let (start, end) = item.split_once("-").unwrap();
+        let start = start.parse::<u64>().unwrap();
+        let end = end.parse::<u64>().unwrap();
+
+        min = min.min(start);
+        max = max.max(end);
+    }
+
+    println!("Fresh: {}", max - min);
 }
