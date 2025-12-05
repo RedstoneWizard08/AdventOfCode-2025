1e05c3e3eb0b7d5e19f9be447db7eccd713bc769
RedstoneWizard08 <jacob1coder@gmail.com>
2025-12-05 12:58:24 -0800

    Cool macro experimentation lol

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,11 +1,14 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::quote;
 use std::{env, fs, path::PathBuf};
-use syn::{LitStr, parse_macro_input};
+use syn::{
+    LitBool, LitStr, Type,
+    parse::{Parse, ParseStream},
+    parse_macro_input, token,
+};
 
-#[proc_macro]
-pub fn embed_input(input: TokenStream) -> TokenStream {
-    let lit = parse_macro_input!(input as LitStr);
+fn resolve_file_path(lit: LitStr) -> PathBuf {
     let input = lit.value();
 
     let source = match lit.span().unwrap().local_file() {
@@ -24,12 +27,20 @@ pub fn embed_input(input: TokenStream) -> TokenStream {
             .join(input)
     };
 
+    path
+}
+
+#[proc_macro]
+pub fn embed_input(input: TokenStream) -> TokenStream {
+    let lit = parse_macro_input!(input as LitStr);
+    let path = resolve_file_path(lit);
+
     let mut width = 0;
     let mut height = 0usize;
     let mut parts = Vec::new();
 
     let content = fs::read_to_string(path).expect("Cannot read file: ");
-    let lines = content.lines();
+    let lines = content.trim().lines();
 
     for line in lines {
         let chars = line.chars();
@@ -46,6 +57,121 @@ pub fn embed_input(input: TokenStream) -> TokenStream {
         const WIDTH: usize = #width;
         const HEIGHT: usize = #height;
         const INPUT: [[char; WIDTH]; HEIGHT] = [#(#parts),*];
+    }
+    .into()
+}
+
+mod kw {
+    use syn::custom_keyword;
+
+    custom_keyword!(ty);
+    custom_keyword!(inclusive);
+    custom_keyword!(sep);
+}
+
+struct EmbedRangeParams {
+    path: LitStr,
+    ty: Type,
+    inclusive: LitBool,
+    sep: LitStr,
+}
+
+impl Parse for EmbedRangeParams {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let path = input.parse()?;
+        let mut inclusive = LitBool::new(false, Span::call_site());
+        let mut sep = LitStr::new("\n", Span::call_site());
+        let ty = Type::Verbatim(quote! { u64 });
+
+        while !input.is_empty() {
+            let look = input.lookahead1();
+
+            if look.peek(token::Comma) {
+                input.parse::<token::Comma>()?;
+
+                let look = input.lookahead1();
+
+                if look.peek(kw::sep) {
+                    input.parse::<kw::sep>()?;
+                    input.parse::<token::Eq>()?;
+
+                    sep = input.parse()?;
+                } else if look.peek(kw::inclusive) {
+                    input.parse::<kw::inclusive>()?;
+                    input.parse::<token::Eq>()?;
+
+                    inclusive = input.parse()?;
+                } else if look.peek(kw::ty) {
+                    // TODO:
+
+                    // input.parse::<kw::ty>()?;
+                    // input.parse::<token::Eq>()?;
+
+                    // ty = input.parse()?;
+
+                    todo!("Custom types are implemented yet!");
+                } else {
+                    return Err(look.error());
+                }
+            } else {
+                break;
+            }
+        }
+
+        Ok(Self {
+            ty,
+            path,
+            inclusive,
+            sep,
+        })
+    }
+}
+
+#[proc_macro]
+pub fn embed_ranges(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as EmbedRangeParams);
+    let path = resolve_file_path(input.path);
+    let content = fs::read_to_string(path).expect("Cannot read file: ");
+    let sep = input.sep.value();
+    let lines = content.trim().split(&sep).collect::<Vec<_>>();
+    let mut parts = Vec::new();
+
+    let inclusive = if input.inclusive.value {
+        quote! { ..= }
+    } else {
+        quote! { .. }
+    };
+
+    let base_ty = input.ty;
+
+    let ty = if input.inclusive.value {
+        quote! { std::ops::RangeInclusive<#base_ty> }
+    } else {
+        quote! { std::ops::Range<#base_ty> }
+    };
+
+    let len = lines.len();
+
+    for line in lines {
+        let (first, last) = line
+            .split_once("-")
+            .expect(&format!("Cannot split line ('{line}') once on '-': "));
+
+        let first = first
+            .parse::<u64>()
+            .expect(&format!("Failed to parse lhs ('{first}'): "));
+
+        let last = last
+            .parse::<u64>()
+            .expect(&format!("Failed to parse rhs ('{last}'): "));
+
+        parts.push(quote! {
+            #first #inclusive #last
+        });
+    }
+
+    quote! {
+        const INPUT: [#ty; #len] = [#(#parts),*];
     }
     .into()
 }
